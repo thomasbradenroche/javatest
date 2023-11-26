15dad41b772d6db200eb2e481259c9e56ad41e9e
Thomas Roche <troche@linwoodhomes.com>
2023-11-25 21:17:40 -0800

    add function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int x = add(1,2);
+        System.out.println("Hello world: " + x);
+    }
+
+    // This is a simple Java function that adds two integers
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
